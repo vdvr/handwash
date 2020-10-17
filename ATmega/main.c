23f9a2da0dcbb985035d3ef279a00c9f8332051d
D3admous3 <43110723+D3admous3@users.noreply.github.com>
2020-10-17 18:11:36 +0200

    ISR en pkg_read toegevoegd(voorlopig)

--- ATmega/main.c
+++ ATmega/main.c
@@ -1,22 +1,52 @@
-/*
- * les 1 blink.c
- *
- * Created: 19/09/2018 14:32:21
- * Author : r0711421
- */ 
-
 #include <avr/io.h>
-#include "board.h"
+#include <avr/interrupt.h>
+#include "uartNano.h"
+#include "buffer.h"
+
+#define STX 2
+#define ETX 3
+
+char commands[20];
+char arguments[20];
+
+char cmd_start = 0;
+
+char pkg[40];
+
+ISR (USART_RX_vect)
+{
+	buffer_write(UDR0);
+}
+
+void pkg_read()
+{
+    while(buffer_used > 0 && buffer_read() != STX);     // zoek achter package in buffer
+    for (int i = 0; i < 20; i++)
+        {
+            if(buffer_used>0)
+            {
+                char command = buffer_read();
+                commands[i] = command;
+                if(command==0) break;
+            }
+        }
+    for (int i = 0; i < 20; i++)
+        {
+            char argument = buffer_read();
+            arguments[i] = argument;
+            if((argument==ETX) | (argument == 0)) break;
+        }
+
+}
 
 int main(void)
 {
-    setup();
-    while (1) 
-    {
-		ledOn(4);
-		wait();
-		ledOff(4);
-		wait();
-    }
-}
+    uartSetup(57600);
 
+    for (;;)
+    {
+        sei();
+
+    }
+    
+}
