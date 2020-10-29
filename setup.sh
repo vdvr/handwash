c62c3df1c6aa8366c3af443ebb989d1bead95764
Ronan L. Van Der Veken <ronan@ronanvdv.org>
2020-10-29 17:03:30 +0100

    added rpi setup script

---
+++ setup.sh
@@ -0,0 +1,2 @@
+sudo echo 'ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", SYMLINK+="arduino"' >> /etc/udev/rules.d/81-arduino.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
