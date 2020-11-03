3000429967ddc05497a19b2001aff6cfc47839af
Ronan L. Van Der Veken <ronan@ronanvdv.org>
2020-11-03 10:02:13 +0100

    removed all communication (git issue)

--- communication/pkg.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PKGSTRUCT__
-#define __PKGSTRUCT__
-
-struct Pkg {
-	char command[4];
-	char arguments[4];
-};
-#endif
--- communication/serial.h
+++
@@ -1,8 +0,0 @@
-#ifndef __Serial__
-#define __Serial__
-
-int serial_open(const char *device);
-int serial_get_char(const int fd);
-void serial_send_char(const int fd, const unsigned char c);
-
-#endif
