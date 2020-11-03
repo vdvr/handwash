e88d1502f4d0b8a73f60c1a26b2acc48edf2dfd2
D3admous3 <43110723+D3admous3@users.noreply.github.com>
2020-11-03 14:43:26 +0100

    merge

--- ATmega/main.c
+++ ATmega/main.c
@@ -1,10 +1,18 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+<<<<<<< HEAD
 #include "board.h"
 #include "uartNano.h"
 #include "buffer.h"
 #include "pkg.h"
 #include "timer2min.h"
+=======
+#include "uart.h"
+#include "buffer.h"
+#include "pkg.h"
+#include "msg.h"
+#include "peripheral.h"
+>>>>>>> 37a9cca1fb1efec95f40e5f90ae3500fb9913219
 
 // ---------------------------------------
 // STX, ETX and NULL are defined in pkg.h
@@ -25,20 +33,19 @@
 // Global variables end -----------------
 
 
-// ISR (USART_RX_vect)             // interrupt for uart receive
-// {
-// 	buffer_write(UDR0);         // write uart data to ringbuffer
-//     if(UDR0 == ETX)             // listen for ETX --> extra pkg in ringbufffer
-//     {
-//         new_pkg ++;
-//     }
-// }
+ISR (USART_RX_vect)             // interrupt for uart receive
+{
+	rpi_avail = 1;
+	buffer_write(UDR0);         // write uart data to ringbuffer
+    if(UDR0 == ETX)             // listen for ETX --> extra pkg in ringbufffer
+    {
+        new_pkg ++;
+    }
+}
 
 ISR(INT0_vect)
 {
     //pkg_construct("water","123");
-    resetTimer();
-    startTimer();
 }
 
 // ISR(INT1_vect)
@@ -54,26 +61,27 @@ ISR(TIMER1_COMPA_vect)
 
 int main(void)
 {
-    boardSetup();               // configures port D for specific use on prototype board
-                                // also enables INT0 en INT1
-    secTimerSetup();
-    //uartSetup(57600);
+	sei();
+	uartSetup(57600);
 
-    sei();
-    for (;;)
-    {
-        // if(new_pkg) 
-        // {
-        //     pkg_destruct();
-        // }
-        // if(new_msg)
-        // {   
-        //     // code here for interpreting the new msg
-        //     new_msg = 0;
+	for (;;)
+	{
+		if (is_faucet_sensor_set())			// checks if object detected at faucet sensor, copy body for external interrupt
+		{
+			if (!rpi_avail) 
+			{
+				faucet_on_while_sensor_set();
+				continue;
+			}
 
+			send_msg(REQUEST_WATER, "");
 
-        //     pkg_construct("123","123");
-        // }
-    }
-    
+			if (wait_perm_msg(500))
+			{
+				faucet_on_while_sensor_set_with_timeout(2000);
+
+				send_msg(WATER_DONE, "");
+			}
+		}
+	}
 }
