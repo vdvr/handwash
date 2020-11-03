bfba8a54f697b9fa00ef5bf6eb5c8d722d806b64
D3admous3 <43110723+D3admous3@users.noreply.github.com>
2020-11-03 13:54:44 +0100

    file add for timer, timer counts in seconds functions: setup, start/stop, reset

---
+++ ATmega/timer2min.c
@@ -0,0 +1,25 @@
+#include <avr/io.h>
+#include "timer2min.h"
+
+void secTimerSetup(void)
+{
+    TCCR1A = 0;
+    OCR1A = 31249;                      // compare value for 1Hz
+    TIMSK1 |= (1<<OCIE1A);                // compare match interrupt
+    //TCCR1B |= (1<<CS12);              // by connecting prescaler timer starts to count
+}
+
+void stopTimer(void)
+{
+    TCCR1B &= ~(1<<CS12);
+}
+
+void startTimer(void)
+{
+    TCCR1B |= (1<<CS12);
+}
+
+void resetTimer(void)
+{
+    TCNT1 = 0;
+}
---
+++ ATmega/timer2min.h
@@ -0,0 +1,12 @@
+#ifndef TIMER2MIN_H_
+#define TIMER2MIN_H_
+
+void secTimerSetup(void);
+
+void stopTimer(void);
+
+void startTimer(void);
+
+void resetTimer(void);
+
+#endif /* TIMER2MIN_H_ */
