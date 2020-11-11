966f658bf6e5a9a8d4b5f3c78eb1db8e9200ed32
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2020-11-11 17:28:27 +0100

    timestamp struct and functions added

--- ATmega/timer2min.c
+++ ATmega/timer2min.c
@@ -1,12 +1,10 @@
 #include <avr/io.h>
 #include "timer2min.h"
 
-char seconds = 0;
-
 void timerSetup(void)
 {
     TCCR1A = 0;
-    OCR1A = 31249;                      // compare value for 1Hz
+    OCR1A = TIMER_OF_1HZ;                      // compare value for 1Hz
     TIMSK1 |= (1<<OCIE1A);                // compare match interrupt
     //TCCR1B |= (1<<CS12);              // by connecting prescaler timer starts to count
 }
@@ -23,5 +21,33 @@ void startTimer(void)                   // word enkel na setup gebruikt
 
 void resetTimer(void)                   // worden niet gebruikt
 {
+    seconds_now = 0;
     TCNT1 = 0;
+}
+
+void getTime(struct timestamp *time_out)
+{
+    time_out->seconds = seconds_now;
+    time_out->extra = TCNT1;
+    // if TCNT1 overflows between statements, seconds will not b
+}
+
+int checkTimeElapsed(struct timestamp *time, unsigned int seconds, unsigned int extra) 
+{
+    unsigned int total_extra;
+    unsigned int extra_now = TCNT1; // store in case overflows between 2 if statements
+    unsigned int total_seconds = time->seconds + seconds;
+
+    if (total_seconds > seconds_now) 
+    {
+        return 1;
+    }
+
+    total_extra = time->extra + extra; // calculate only if needed
+    if ((total_seconds == seconds_now) || (total_extra >= extra_now))
+    {
+        return 1
+    }
+
+    return 0;
 }
--- ATmega/timer2min.h
+++ ATmega/timer2min.h
@@ -1,7 +1,14 @@
 #ifndef TIMER2MIN_H_
 #define TIMER2MIN_H_
 
-extern char seconds;
+#define TIMER_OF_1HZ 31249
+
+struct timestamp {
+    unsigned int seconds;
+    unsigned int extra;
+};
+
+extern unsigned int seconds_now;
 
 void timerSetup(void);
 
@@ -10,5 +17,9 @@ void stopTimer(void);
 void startTimer(void);
 
 void resetTimer(void);
+
+void getTime(struct timestamp *time_out);
+
+int checkTimeElapsed(struct timestamp *time, unsigned int seconds, unsigned int extra);
 
 #endif /* TIMER2MIN_H_ */
