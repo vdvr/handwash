eec7381b0ad55a8d78db07f05bef6647fda44786
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2020-11-18 19:56:32 +0100

    disable screen timeout added to setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
+sudo apt install libvlc-dev
+
+# disable screen timeout
+sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart > /dev/null <<EOF
+@xset s noblank
+@xset s off
+@xset -dpms
+EOF
+
 sudo echo 'ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", SYMLINK+="arduino"' >> /etc/udev/rules.d/81-arduino.rules
 sudo udevadm control --reload-rules && sudo udevadm trigger
