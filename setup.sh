4bc4bd16a99608212fe394393c509f304976f22c
vdlluca <luca.vanderlinden@student.ucll.be>
2020-11-19 02:51:01 +0100

    ignore compiled binaries and make on setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 sudo echo 'ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", SYMLINK+="arduino"' >> /etc/udev/rules.d/81-arduino.rules
 sudo udevadm control --reload-rules && sudo udevadm trigger
+
+make -C rpicom all
