5a6355fcd1465583263ac38dfe5fbfdcc0701bba
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2020-12-25 08:25:36 +0100

    resize microbes during initialization

--- microbe_ar/main.py
+++ microbe_ar/main.py
@@ -39,10 +39,11 @@ def microbe_ar_feed():
         hand, conn_loc = possible_locations[loc_i]
         possible_locations.pop(loc_i)
 
-        img_nr = random.randint(0, len(microbe_imgs) - 1)
-        init_height, init_width = microbe_imgs[img_nr].shape[:2]
+        microbe_img_nr = random.randrange(len(microbe_imgs))
+        init_height, init_width = microbe_imgs[microbe_img_nr].shape[:2]
         height = MICROBE_MIN_HEIGHT + random.randint(0, MICROBE_MAX_HEIGHT - MICROBE_MIN_HEIGHT)
         width = int(height / init_height * init_width)
+        microbe_img = cv.resize(microbe_imgs[microbe_img_nr], (width, height))
 
         microbe_data[hand].append(
             {
@@ -56,13 +57,8 @@ def microbe_ar_feed():
                     "prev_y": None,
                     "prev_angle": None,
                 },
-                "size": 
-                {
-                    "width": width,
-                    "height": height,
-                },
                 "opacity": 1,
-                "image_nr": random.randint(0, len(microbe_imgs) - 1),
+                "image": microbe_img,
             }
         )
 
@@ -129,18 +125,17 @@ def microbe_ar_feed():
                     microbe["opacity"] -= OPACITY_DECR
 
                 # resize image
-                microbe_img = microbe_imgs[microbe["image_nr"]]
-                microbe_img = cv.resize(microbe_img, (microbe["size"]["width"], microbe["size"]["width"]))
+                microbe_img = microbe["image"]
 
                 # if hand detected, use landmark connections
                 if hand_i != None:
                     landmark_result = results.multi_hand_landmarks[hand_i]
-                    image = draw_microbe_on_hand(image, microbe_img, landmark_result, microbe)
+                    image = draw_microbe_on_hand(image, microbe["image"], landmark_result, microbe)
 
                 # if hand not detected, use motion
                 else:
                     try:
-                        image = draw_microbe_on_motion(image, microbe_img, motion_pos, microbe)
+                        image = draw_microbe_on_motion(image, microbe["image"], motion_pos, microbe)
                     # error when motion_pos None (no movement), likely no hands
                     except TypeError:
                         pass
